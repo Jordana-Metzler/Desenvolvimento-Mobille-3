ee16631f8ce4d0426fb470f17aaea1b8d055bd9c
unknown <Administrador@lci-15-10.upf.br>
2025-05-21 21:57:26 -0300

    implementação da home page + formulário

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.deepPurpleAccent,
+          foregroundColor: Colors.white,
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: LoginPage(),
